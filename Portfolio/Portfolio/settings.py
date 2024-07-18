e5057acf374fe85e952821eb5af1133fc0bf28a8
Chirag Singh Chhonkar <Chhonkarchirag886@gmail.com>
2024-07-18 16:10:09 +0530

    commit

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Portfolio.Portfolio.project',
+    'Portfolio.project',
     'Portfolio.contactform',
     'Portfolio.Aboutme',
     'Portfolio.Services',
